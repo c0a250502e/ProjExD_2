f99591768a0430e860fd5bd267b608af0dc42f6c
yuzuki <c0a250502e@edu.teu.ac.jp>
2026-04-21 17:07:52 +0900

    改行

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -66,6 +66,23 @@ def init_bb_imgs() -> tuple[list[pg.Surface], list[int]]:
     bb_accs = [a for a in range(1, 11)]
     return bb_imgs, bb_accs
 
+def get_kk_imgs() -> dict[tuple[int, int], pg.Surface]:
+    base_img = pg.image.load("fig/3.png")
+    # 左向きが基本なので、左右反転(flip)させたものを右向きの基本にする
+    base_img_flip = pg.transform.flip(base_img, True, False)
+    
+    kk_dict = {
+        ( 0,  0): pg.transform.rotozoom(base_img_flip, 0, 1.0),   # 止まっている（右向き）
+        (+5,  0): pg.transform.rotozoom(base_img_flip, 0, 1.0),   # 右
+        (+5, -5): pg.transform.rotozoom(base_img_flip, 45, 1.0),  # 右上
+        ( 0, -5): pg.transform.rotozoom(base_img_flip, 90, 1.0),  # 上
+        (-5, -5): pg.transform.rotozoom(base_img, -45, 1.0),      # 左上
+        (-5,  0): pg.transform.rotozoom(base_img, 0, 1.0),        # 左
+        (-5, +5): pg.transform.rotozoom(base_img, 45, 1.0),       # 左下
+        ( 0, +5): pg.transform.rotozoom(base_img, -90, 1.0),      # 下
+        (0, +5): pg.transform.rotozoom(base_img_flip, -45, 1.0), # 右下
+    }
+    return kk_dict
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
@@ -76,6 +93,7 @@ def main():
     kk_rct.center = 300, 200
 
     bb_imgs, bb_accs = init_bb_imgs()
+    kk_imgs = get_kk_imgs()
 
     bb_img = pg.Surface((20,20))
     pg.draw.circle(bb_img, (225, 0, 0),(10,10),10)
@@ -100,9 +118,9 @@ def main():
 
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
-        avx = vx * bb_accs[min(tmr // 50, 9)]
-        avy = vy * bb_accs[min(tmr // 50, 9)]
-        bb_img = bb_imgs[min(tmr // 50, 9)]
+        avx = vx * bb_accs[min(tmr // 500, 9)]
+        avy = vy * bb_accs[min(tmr // 500, 9)]
+        bb_img = bb_imgs[min(tmr // 500, 9)]
         bb_rct.width = bb_img.get_rect().width
         bb_rct.height = bb_img.get_rect().height
         # if key_lst[pg.K_UP]:
@@ -117,22 +135,26 @@ def main():
             if key_lst[key]:
                 sum_mv[0] += mv[0]
                 sum_mv[1] += mv[1]
+        if tuple(sum_mv) in kk_imgs:
+            kk_img = kk_imgs[tuple(sum_mv)]
         kk_rct.move_ip(sum_mv)
-        if check_bound(kk_rct) != (True, True):
-            kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
+        yoko, tate = check_bound(kk_rct)
+        if not yoko:
+            kk_rct.move_ip(-sum_mv[0], 0)
+        if not tate:
+            kk_rct.move_ip(0, -sum_mv[1])
+
         screen.blit(kk_img, kk_rct)
-        bb_rct.move_ip(avx,avy)
-        yoko,tate = check_bound(bb_rct)
+        bb_rct.move_ip(avx, avy)
+        yoko, tate = check_bound(bb_rct)
         if not yoko:
             vx *= -1
         if not tate:
-            vy *=-1
+            vy *= -1
         screen.blit(bb_img, bb_rct)
         pg.display.update()
         tmr += 1
         clock.tick(50)
-
-
 
 
 if __name__ == "__main__":
